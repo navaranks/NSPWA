ab1f2f867f082e78edccd208665ec7dfc36a69b9
Chris <71299080+navaranks@users.noreply.github.com>
2023-07-04 23:16:43 -0700

    more initial commits

--- scripts/custom.js
+++ scripts/custom.js
@@ -1437,6 +1437,63 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             }
         }
+        //Import HTML Files
+        /*window.onload = function() {
+            var elements = document.getElementsByTagName('*'),
+                i;
+            for (i in elements) {
+                if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
+                    fragment(elements[i], elements[i].getAttribute('data-include'), elements[i].getAttribute('data-include-callback'));
+                }
+            }
+            function getFuncByName(functionName) {
+                var context = window;
+                var namespaces = functionName.split(".");
+                var func = namespaces.pop();
+                for(var i=0; i < namespaces.length; i+=1) {
+                    var ns = namespaces[i];
+                    if (i === 0 && ns === 'window') {
+                        continue;
+                    }
+                    context = context[ns];
+                }
+                return context[func];
+            }
+            function fragment(el, url, callbackName) {
+                var localTest = /^(?:file):/,
+                    xmlhttp = new XMLHttpRequest(),
+                    status = 0;
+        
+                xmlhttp.onreadystatechange = function() {
+                   
+                    if (xmlhttp.readyState == 4) {
+                        status = xmlhttp.status;
+                    }
+                    if (localTest.test(location.href) && xmlhttp.responseText) {
+                        status = 200;
+                    }
+                    if (xmlhttp.readyState == 4 && status == 200) {
+                        var parentElement = el.parentElement;
+                        
+                        el.outerHTML = xmlhttp.responseText;
+                        
+                        if (!!callbackName) {
+                            var callback = getFuncByName(callbackName);
+                            if (typeof callback === 'function') {
+                                callback(parentElement, xmlhttp);
+                            }
+                        }
+                    }
+                }
+        
+                try { 
+                    xmlhttp.open("GET", url, true);
+                    xmlhttp.send();
+                } catch(err) {
+                    
+                }
+            }
+        } */
     }
 
     //Fix Scroll for AJAX pages.
